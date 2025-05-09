3ddd3a8a66906b3a4edbe877dfd2378e82fca3be
qozeem monsurudeen <qozeemmonsurudeen@gmail.com>
2025-05-09 08:50:52 -0700

    https added using URL::Scheme

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -422,6 +422,7 @@ class UserApiController extends Controller
     {
         try {
             $user = $request->user();
+            return $user;
             $infoUser = db('drivers')->where('id', $user->record_id)->updateRecord(['image' => $request->file]);
             if ($infoUser->status == true) {
                 $driver = db('drivers')->withRelations(['documents', 'car_details', 'wallet', 'info_bank'])->findRecord($infoUser->data->id);
